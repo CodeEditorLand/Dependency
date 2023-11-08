23429a9ad6d4e479f851b08f9aeeb874ffd2784c
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-09 00:49:21 +0200

    squash!

--- Script/Configure/Repository.sh
+++ Script/Configure/Repository.sh
@@ -7,18 +7,18 @@ Directory=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 readarray -t Repository < "$Directory"/../Cache/Repository/CodeEditorLand.md
 
 for Repository in "${Repository[@]}"; do
-	Current="$(git config --get remote.origin.url)"
 	Folder="${Repository/'CodeEditorLand/'/}"
 
 	cd "${Folder}" || exit
 
 	gh repo set-default "$(git remote get-url origin)"
 
-	Upstream=$(gh repo view --json parent | jq --compact-output -r '.parent.owner.login, .parent.name')
+	Upstream=$(gh repo view --json parent | jq --compact-output -r '.parent.owner.login, .parent.name' | tr -s '\r\n' '/')
+	Upstream="ssh://git@github.com/${Upstream}"
+	Upstream=$(echo "$Upstream" | sed 's/\/$/\.git/')
 
-	echo "$Upstream"
-	# git remote add upstream "$Upstream"
-	# git remote set-url upstream "$Upstream"
+	git remote add upstream "$Upstream"
+	git remote set-url upstream "$Upstream"
 
 	cd - || exit
 done
