08f4a87279e79013c9eb05efea8eca9341356a2e
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-08 16:34:53 +0200

    squash!

--- Script/Rename/Directory.sh
+++ Script/Rename/Directory.sh
@@ -7,22 +7,33 @@ Directory=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 readarray -t Repository < "$Directory"/../Cache/Repository/CodeEditorLand.md
 
 for Repository in "${Repository[@]}"; do
-	echo "$Repository"
-	
-	# gh repo rename 
+	Folder="${Repository/'CodeEditorLand/'/}"
+
+	Rename=""
+
+	Rename=$(tr '[:lower:]' '[:upper:]' <<< "${Folder:0:1}")
+
+	for ((i = 1; i < ${#Folder}; i++)); do
+		if [ "${Folder:i:1}" = "-" ]; then
+			Next="${Folder:i+1:1}"
+			if [[ "$Next" =~ [a-z] ]]; then
+				Upper=$(tr '[:lower:]' '[:upper:]' <<< "$Next")
+				Rename="${Rename}${Upper}"
+				((i++))
+			else
+				Rename="${Rename}-"
+			fi
+		else
+			Rename="${Rename}${Folder:i:1}"
+		fi
+	done
+
+	Rename=$(echo "$Rename" | sed -E "s/vscode/Land/gI")
+
+	cd "$Folder" || exit
+
+	gh repo set-default "$(git remote get-url origin)"
+	gh repo rename "$Rename" --yes
+
+	cd - || exit
 done
-
-
-# USAGE
-#   gh repo rename [<new-name>] [flags]
-
-# FLAGS
-#   -R, --repo [HOST/]OWNER/REPO   Select another repository using the [HOST/]OWNER/REPO format
-#   -y, --yes                      Skip the confirmation prompt
-
-# INHERITED FLAGS
-#   --help   Show help for command
-
-# LEARN MORE
-#   Use 'gh <command> <subcommand> --help' for more information about a command.
-#   Read the manual at https://cli.github.com/manual
