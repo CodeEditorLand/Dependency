ea4c3bab384f459ae95fe054c1082051ef805c96
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-16 02:05:10 +0200

    squash!

---
+++ Script/Rename/Branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Context: CodeEditorLand/Application
+
+Directory=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+readarray -t Repository <"$Directory"/../Cache/Repository/CodeEditorLand.md
+
+for Repository in "${Repository[@]}"; do
+	Folder="${Repository/'CodeEditorLand/'/}"
+
+	cd "$Folder" || exit
+
+	pwd
+
+	gh repo set-default "$(git remote get-url origin)"
+
+	git switch -c main
+
+	git push -f --set-upstream origin main
+	
+	gh repo edit --default-branch main
+
+	cd - || exit
+done
