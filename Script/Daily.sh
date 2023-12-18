b20cc054bf2c3f0dec55ccd898f22bacb6f98618
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-18 08:05:11 +0200

    squash!

--- Script/Daily.sh
+++ Script/Daily.sh
@@ -18,8 +18,10 @@ Directory=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && \pwd)
 
 "$Directory"/Sync/Repository.sh
 
-"$Directory"/Clean/Repository.sh
+# "$Directory"/Clean/Repository.sh
 # "$Directory"/Clean/Detail.sh
 "$Directory"/Move/src.sh
 "$Directory"/Move/license.sh
 # "$Directory"/Append/Detail.sh
+
+Maintain Workflow
