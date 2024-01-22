f0a156ece1a1b2279e7445595eabed09158cb64d
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-22 09:53:53 +0200

    squash!

--- Script/Daily.sh
+++ Script/Daily.sh
@@ -9,20 +9,20 @@ Directory=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && \pwd)
 "$Directory"/Cache/Get.sh
 "$Directory"/Module/Git.sh
 
-# "$Directory"/Clone/Repository.sh
+"$Directory"/Clone/Repository.sh
 
-# "$Directory"/Configure/Repository.sh
-# "$Directory"/Setting/Repository.sh
-# "$Directory"/Rename/Repository.sh
-# "$Directory"/Rename/Branch.sh
+"$Directory"/Configure/Repository.sh
+"$Directory"/Setting/Repository.sh
+"$Directory"/Rename/Repository.sh
+"$Directory"/Rename/Branch.sh
 
 "$Directory"/Sync/Repository.sh
 
-# "$Directory"/Clean/Repository.sh
-# "$Directory"/Clean/Detail.sh
+"$Directory"/Clean/Repository.sh
+"$Directory"/Clean/Detail.sh
 "$Directory"/Move/license.sh
 "$Directory"/Move/package.sh
 "$Directory"/Move/src.sh
-# "$Directory"/Append/Detail.sh
+"$Directory"/Append/Detail.sh
 
 Maintain Workflow
