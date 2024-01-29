1498fd93815c6a450d4492099351ed74e8102745
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-29 18:00:50 +0200

    squash!

--- Source/Fn/Daily.rs
+++ Source/Fn/Daily.rs
@@ -4,25 +4,26 @@ fn main() {
 	println!("Process: Daily.rs");
 
 	// Context: CodeEditorLand/Application
-	let directory = env::current_dir().expect("Failed to get current directory");
+	let current = env::current_dir().expect("Failed to get current directory");
 
-	run_script(&directory.join("Cache").join("Get.sh"));
-	run_script(&directory.join("Clone").join("Repository.sh"));
-	run_script(&directory.join("Module").join("Git.sh"));
-	run_script(&directory.join("Configure").join("Repository.sh"));
-	run_script(&directory.join("Setting").join("Repository.sh"));
-	run_script(&directory.join("Rename").join("Repository.sh"));
-	run_script(&directory.join("Rename").join("Branch.sh"));
-	run_script(&directory.join("Sync").join("Repository.sh"));
-	run_script(&directory.join("Clean").join("Repository.sh"));
-	run_script(&directory.join("Clean").join("Detail.sh"));
-	run_script(&directory.join("Move").join("license.sh"));
-	run_script(&directory.join("Move").join("package.sh"));
-	run_script(&directory.join("Move").join("src.sh"));
-	run_script(&directory.join("Append").join("Detail.sh"));
-	run_script(&directory.join("Sort").join("Detail.sh"));
+	run_script(&current.join("Cache").join("Get.sh"));
+	run_script(&current.join("Clone").join("Repository.sh"));
+	run_script(&current.join("Module").join("Git.sh"));
+	run_script(&current.join("Configure").join("Repository.sh"));
+	run_script(&current.join("Setting").join("Repository.sh"));
+	run_script(&current.join("Rename").join("Repository.sh"));
+	run_script(&current.join("Rename").join("Branch.sh"));
+	run_script(&current.join("Sync").join("Repository.sh"));
+	run_script(&current.join("Clean").join("Repository.sh"));
+	run_script(&current.join("Clean").join("Detail.sh"));
+	run_script(&current.join("Move").join("license.sh"));
+	run_script(&current.join("Move").join("package.sh"));
+	run_script(&current.join("Move").join("src.sh"));
+	run_script(&current.join("Append").join("Detail.sh"));
+	run_script(&current.join("Sort").join("Detail.sh"));
 
-	maintain_workflow();
+	// TODO: Execute Maintain Workflow
+	// Maintain Workflow
 }
 
 fn run_script(script_path: &Path) {
@@ -32,8 +33,4 @@ fn run_script(script_path: &Path) {
 	} else {
 		println!("Script not found: {:?}", script_path);
 	}
-}
-
-fn maintain_workflow() {
-	println!("Maintain Workflow");
 }
--- Source/Fn/Sort/Detail.rs
+++ Source/Fn/Sort/Detail.rs
@@ -4,12 +4,17 @@ fn main() {
 	println!("Process: Sort/Detail.sh");
 
 	// Context: CodeEditorLand/Application
-	let directory = std::env::current_dir().expect("Failed to get current directory");
-	let cache_path = directory.join("../Cache/Repository/Build.md");
 
-	let repositories = read_array(&cache_path).expect("Failed to read repositories");
-
-	for repository in repositories {
+	for repository in std::fs::read_to_string(
+		std::env::current_dir()
+			.expect("Failed to get current directory")
+			.join("../Cache/Repository/Build.md"),
+	)?
+	.lines()
+	.map(|s| s.to_string())
+	.collect()
+	.expect("Failed to read repositories")
+	{
 		let folder = repository.replace("CodeEditorLand/", "");
 
 		let output =
@@ -62,10 +67,4 @@ fn main() {
 
 		println!("{}", String::from_utf8_lossy(&output.stdout));
 	}
-}
-
-fn read_array(file_path: &std::path::Path) -> Result<Vec<String>, std::io::Error> {
-	let content = std::fs::read_to_string(file_path)?;
-	let repositories: Vec<String> = content.lines().map(|s| s.to_string()).collect();
-	Ok(repositories)
 }
