35340f7ee1e05067b6e14b0acb6ba11e55b78301
Nikola Hristov <nikola@nikolahristov.tech>
2024-02-10 04:35:13 +0200

    squash!

--- Script/Daily.sh
+++ Script/Daily.sh
@@ -6,22 +6,22 @@
 
 Directory=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && \pwd)
 
-"$Directory"/Cache/Get.sh
+# "$Directory"/Cache/Get.sh
 
 # "$Directory"/Clone/Repository.sh
-"$Directory"/Module/Git.sh
+# "$Directory"/Module/Git.sh
 
-"$Directory"/Configure/Repository.sh
+# "$Directory"/Configure/Repository.sh
 # "$Directory"/Setting/Repository.sh
 # "$Directory"/Rename/Repository.sh
 
 # "$Directory"/Sync/Repository.sh
-"$Directory"/Switch/Branch/Stream.sh
-"$Directory"/Reset/Repository.sh
-"$Directory"/Switch/Branch/Repository.sh
-"$Directory"/Reset/Repository.sh
+# "$Directory"/Switch/Branch/Stream.sh
+# "$Directory"/Reset/Repository.sh
+# "$Directory"/Switch/Branch/Repository.sh
+# "$Directory"/Reset/Repository.sh
 
-"$Directory"/Default/Branch/Repository.sh
+# "$Directory"/Default/Branch/Repository.sh
 
 # "$Directory"/Clean/Repository.sh
 # "$Directory"/Clean/Detail.sh
