4d1e5ee7aa60b04a5d6856c3832610f031b6cf55
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-09 18:55:29 +0200

    squash!

--- Maintain/Switch/Branch.sh
+++ Maintain/Switch/Branch.sh
@@ -2,28 +2,12 @@
 
 Current=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && \pwd)
 
+# shellcheck disable=SC1091
+source "$Current"/../Fn/Argument.sh
+
+Fn "$@"
+
 if [ $# -gt 0 ]; then
-	if [ -f "$1" ]; then
-		\mapfile -t Organization < <(jq -r '.[]' "$1" | \tr -d '\r')
-	else
-		\echo "Cannot Organization."
-		\exit 1
-	fi
-
-	if [ -f "$2" ]; then
-		\mapfile -t Service < <(jq -r '.[]' "$2" | \tr -d '\r')
-	else
-		\echo "Cannot Service."
-		\exit 1
-	fi
-
-	if [ -n "$3" ]; then
-		Foundation=$3
-	else
-		\echo "Cannot Foundation."
-		\exit 1
-	fi
-
 	if [ -n "$4" ]; then
 		Branch=$4
 	else
