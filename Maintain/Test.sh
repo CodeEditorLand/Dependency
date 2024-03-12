d06da08f2b27430792427e2a1f78fb5618619caf
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-03-12 14:32:23 +0200

    squash!

---
+++ Maintain/Test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Current=$(\cd -- "$(\dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && \pwd)
+
+Foundation=(
+	"None"
+)
+
+for Foundation in "${Foundation[@]}"; do
+	Cache="$Current"/Cache
+
+	Organization="$Cache"/Organization/"$Foundation".json
+	Omit="$Cache"/Omit/"$Foundation".json
+	Service="$Cache"/Service/"$Foundation".json
+
+	"$Current"/Cache/Service.sh \
+		"$Organization" \
+		"$Omit" \
+		"$Foundation"
+
+	"$Current"/Setting/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
+done
