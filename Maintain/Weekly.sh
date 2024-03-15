c1b9a6685945d45a0a1f25489ec955dfd9a2618a
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-15 14:35:58 +0200

    squash!

--- Maintain/Weekly.sh
+++ Maintain/Weekly.sh
@@ -26,109 +26,109 @@ for Foundation in "${Foundation[@]}"; do
 		"$Service" \
 		"$Foundation"
 
-	"$Current"/Configure/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Setting/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Rename/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	# "$Current"/Sync/Service.sh \
+	# "$Current"/Configure/Service.sh \
 	# 	"$Organization" \
 	# 	"$Service" \
 	# 	"$Foundation"
 
-	"$Current"/Switch/Branch.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation" \
-		"Previous"
-
-	"$Current"/Reset/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Switch/Branch.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation" \
-		"Current"
-
-	"$Current"/Reset/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Default/Branch.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation" \
-		"Current"
-
-	"$Current"/Move/license.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Move/package.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Move/src.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Clean/Service.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Clean/Detail.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Append/Detail.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	"$Current"/Sort/Detail.sh \
-		"$Organization" \
-		"$Service" \
-		"$Foundation"
-
-	# "$Current"/Restore/.gitignore.sh \
+	# "$Current"/Setting/Service.sh \
 	# 	"$Organization" \
 	# 	"$Service" \
 	# 	"$Foundation"
 
-	# "$Current"/Restore/package.json.sh \
+	# "$Current"/Rename/Service.sh \
 	# 	"$Organization" \
 	# 	"$Service" \
 	# 	"$Foundation"
 
-	# "$Current"/Restore/src.sh \
+	# # "$Current"/Sync/Service.sh \
+	# # 	"$Organization" \
+	# # 	"$Service" \
+	# # 	"$Foundation"
+
+	# "$Current"/Switch/Branch.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation" \
+	# 	"Previous"
+
+	# "$Current"/Reset/Service.sh \
 	# 	"$Organization" \
 	# 	"$Service" \
 	# 	"$Foundation"
+
+	# "$Current"/Switch/Branch.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation" \
+	# 	"Current"
+
+	# "$Current"/Reset/Service.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Default/Branch.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation" \
+	# 	"Current"
+
+	# "$Current"/Move/license.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Move/package.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Move/src.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Clean/Service.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Clean/Detail.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Append/Detail.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# "$Current"/Sort/Detail.sh \
+	# 	"$Organization" \
+	# 	"$Service" \
+	# 	"$Foundation"
+
+	# # "$Current"/Restore/.gitignore.sh \
+	# # 	"$Organization" \
+	# # 	"$Service" \
+	# # 	"$Foundation"
+
+	# # "$Current"/Restore/package.json.sh \
+	# # 	"$Organization" \
+	# # 	"$Service" \
+	# # 	"$Foundation"
+
+	# # "$Current"/Restore/src.sh \
+	# # 	"$Organization" \
+	# # 	"$Service" \
+	# # 	"$Foundation"
 done
 
-\cd "$Current"/../ || \exit
+# \cd "$Current"/../ || \exit
 
-\biome check . --apply
+# \biome check . --apply
 
-Maintain Workflow
+# Maintain Workflow
 
-\cd - || \exit
+# \cd - || \exit
