13d75aa78e83177e8b5df975765885a42e1b5f20
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-15 12:57:20 +0200

    squash!

--- Maintain/Fn/Setting/Service.sh
+++ Maintain/Fn/Setting/Service.sh
@@ -13,13 +13,15 @@ fi
 	--method DELETE \
 	-H "Accept: application/vnd.github+json" \
 	-H "X-GitHub-Api-Version: 2022-11-28" \
-	repos/"${Service}"/branches/Current/protection
+	repos/"${Service}"/branches/Current/protection \
+	--silent
 
 \gh api \
 	--method DELETE \
 	-H "Accept: application/vnd.github+json" \
 	-H "X-GitHub-Api-Version: 2022-11-28" \
-	repos/"${Service}"/branches/Previous/protection
+	repos/"${Service}"/branches/Previous/protection \
+	--silent
 
 \gh api \
 	--method PUT \
