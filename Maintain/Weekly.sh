4662a07874b081714797d8f9a776d85f5b23ee32
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-09 16:02:38 +0300

    squash!
    Signed-off-by: Nikola R. Hristov <nikola@nikolahristov.tech>

--- Maintain/Weekly.sh
+++ Maintain/Weekly.sh
@@ -21,108 +21,108 @@ for Foundation in "${Foundation[@]}"; do
 		"$Omit" \
 		"$Foundation"
 
-	# "$Current"/Module/Foundation.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Module/Foundation.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Configure/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Configure/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Setting/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Setting/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Rename/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Rename/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
 	# # "$Current"/Sync/Service.sh \
 	# # 	"$Organization" \
 	# # 	"$Service" \
 	# # 	"$Foundation"
 
-	# "$Current"/Save/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Save/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Switch/Branch.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation" \
-	# 	"Previous"
+	"$Current"/Switch/Branch.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation" \
+		"Previous"
+
+	"$Current"/Reset/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
+
+	"$Current"/Switch/Branch.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation" \
+		"Current"
+
+	"$Current"/Save/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
 	# "$Current"/Reset/Service.sh \
 	# 	"$Organization" \
 	# 	"$Service" \
 	# 	"$Foundation"
 
-	# "$Current"/Switch/Branch.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation" \
-	# 	"Current"
+	"$Current"/Default/Branch.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation" \
+		"Current"
 
-	# "$Current"/Save/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Move/license.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Reset/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Move/package.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Default/Branch.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation" \
-	# 	"Current"
-
-	# "$Current"/Move/license.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
-
-	# "$Current"/Move/package.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
-
-	# "$Current"/Move/src.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Move/src.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
 	"$Current"/Clean/Service.sh \
 		"$Organization" \
 		"$Service" \
 		"$Foundation"
 
-	# "$Current"/Clean/Detail.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Clean/Detail.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Append/Detail.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Append/Detail.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Sort/Detail.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Sort/Detail.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
-	# "$Current"/Save/Service.sh \
-	# 	"$Organization" \
-	# 	"$Service" \
-	# 	"$Foundation"
+	"$Current"/Save/Service.sh \
+		"$Organization" \
+		"$Service" \
+		"$Foundation"
 
 	# "$Current"/Restore/.gitignore.sh \
 	# 	"$Organization" \
