7755416b2ba19897c1f88ea806b8eef8840405b9
Nikola R. Hristov <nikola@playform.cloud>
2024-04-22 18:20:29 +0300

    squash!

--- Maintain/Fn/Configure/Remote.sh
+++ Maintain/Fn/Configure/Remote.sh
@@ -2,13 +2,18 @@
 
 \pwd
 
-URL() {
+Remote() {
 	git remote get-url "$1" 2>/dev/null || git remote get-url origin
 }
 
-Source=$(URL Source | \sed 's/git@github.com:/ssh:\/\/git@github.com\//')
+Source=$(Remote Source)
+
+Source=$(\echo "$Source" | \sed 's/git@github.com:/ssh:\/\/git@github.com\//')
 
 \git remote set-url Source "$Source"
+
+\echo "Source: "
+\echo "$Source"
 
 \gh repo set-default "$(\git remote get-url Source)"
 
@@ -17,7 +22,11 @@ Parent=$(\gh repo view --json parent | \jq -c -r '.parent.owner.login, .parent.n
 \git remote remove Parent
 
 if [[ "$Parent" != "null/null" && "$Parent" != "null/null/" ]]; then
-	Parent=$("ssh://git@github.com/${Parent}" | \sed 's/\/$/\.git/')
+	Parent="ssh://git@github.com/${Parent}"
+	Parent=$(\echo "$Parent" | \sed 's/\/$/\.git/')
+
+	\echo "Parent: "
+	\echo "$Parent"
 
 	\git remote add Parent "$Parent"
 	\git remote set-url Parent "$Parent"
