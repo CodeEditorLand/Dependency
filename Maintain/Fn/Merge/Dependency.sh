2bfc066f9ce7dd717c9ccec3b570b9c939383096
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-15 03:05:13 +0300

    squash!

--- Maintain/Fn/Merge/Dependency.sh
+++ Maintain/Fn/Merge/Dependency.sh
@@ -2,6 +2,6 @@
 
 \pwd
 
-\git fetch Parent --all --no-tags --unshallow
+\git fetch Parent --no-tags --unshallow
 
 \git merge Parent/"$(\gh repo view "$(\gh repo view --json parent | \jq -c -r '.parent.owner.login, .parent.name' | \tr -s '\r\n' '/' | \sed 's/\/$//')" --json defaultBranchRef | \jq -r -c '.defaultBranchRef.name')" --no-edit --allow-unrelated-histories -X theirs
