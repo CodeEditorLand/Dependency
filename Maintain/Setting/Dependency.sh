0b02d19a5dd69b924acf48322965b1fbb2d38a2f
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-05-19 22:02:23 +0300

    fix(Maintain): Correct escape sequences in Dependency.sh for consistency
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Maintain/Setting/Dependency.sh
+++ Maintain/Setting/Dependency.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env sh
 
-Current=$(cd -- "$(dirname -- "$0")" > /dev/null 2>&1 && pwd)
+Current=$(\cd -- "$(\dirname -- "$0")" > /dev/null 2>&1 && \pwd)
 
 _FN_DIR_="$Current/../Fn"
-export _FN_DIR_
+\export _FN_DIR_
 
 # shellcheck disable=SC1091
 . "$Current/../Fn/Argument.sh"
 
 Fn "$@"
 
-while IFS= read -r Organization; do
+while IFS= \read -r Organization; do
 	(
-		while IFS= read -r SubDependency; do
+		while IFS= \read -r SubDependency; do
 			("$Current/../Fn/Setting/Dependency.sh" "$SubDependency") &
 		done <<- EOF
 			$SubDependency
 		EOF
 
-		wait
+		\wait
 	) &
 done <<- EOF
 	$Organization
 EOF
 
-wait
+\wait
